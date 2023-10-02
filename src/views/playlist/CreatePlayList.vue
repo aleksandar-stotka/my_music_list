8fe81b9cd06e16e1d7e12764d52e1aafa3c81df4
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-10-02 15:27:54 -0700

    upload-image-in-firebase

--- src/views/playlist/CreatePlayList.vue
+++ src/views/playlist/CreatePlayList.vue
@@ -7,8 +7,10 @@
 
    <label>Upload playlist cover image</label>
    <input type="file" @change="handleChange">
+   <div class="error">{{ fileError }}</div>
+
    <div class="error"></div>
-   <button>create</button>
+   <button>Create</button>
    
  </form>
 </template>
@@ -16,24 +18,42 @@
 <script>
 import { ref } from 'vue'
 
+import useStorage from "@/composables/useStorage"
+
+
 export default {
     setup() {
+   
+           const {url, filePath, uploadImage} = useStorage()
         const title = ref('')
         const description = ref("")
         const file = ref(null)
+        const fileError = ref(null)
 
-        const  handleSubmit = () => {
-            console.log(title.value, description.value)
+        const  handleSubmit = async () => {
+           await uploadImage(file.value)
+           console.log("image upload url:", url.value)
         }
+        //ALLOWED TYPES FILE 
+
+        const  types = ['image/png', 'image/jpeg']
 
         const handleChange = (e) => {
 
-            const select = e.target.files
+            const select = e.target.files[0]
             console.log(select)
+
+            if(select && types.includes(select.type)) {
+                file.value = select
+                fileError.value =null
+            } else {
+                file.value = null
+                fileError.value = "Please select an image file (png or jbg)"
+            }
 
         }
 
-        return {title,description, file, handleSubmit, handleChange}
+        return {title,description, file, handleSubmit, handleChange, types, fileError, filePath}
     }
 
 
